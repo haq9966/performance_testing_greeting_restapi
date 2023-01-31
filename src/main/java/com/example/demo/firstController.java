238674ba4f1661d338d02a3fadafd89465960c47
Abdul Hakeem <75352223+haq9966@users.noreply.github.com>
2023-01-31 15:04:27 +0530

    Update firstController.java

--- src/main/java/com/example/demo/firstController.java
+++ src/main/java/com/example/demo/firstController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class firstController {
     
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String first_api(){
         return "Welcome to the app.";
-    }
-    @GetMapping("/")
-    public String second_api(){
-            return "Home.";    
     }
 }
